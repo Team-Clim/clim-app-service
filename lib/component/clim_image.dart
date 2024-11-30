5e429fd9dac6864aa4fa5a7eb322157f09a8aa5a
taejun <ptj01036251@gmail.com>
2024-11-30 12:53:18 +0900

    Clim images 관리

---
+++ lib/component/clim_image.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ClimImage {
+  static Image climLogo = Image.asset('assets/images/logo/clim_logo.png');
+  static Image climLogo2 = Image.asset('assets/images/logo/clim_logo2.png');
+
+
+
+  static Image backImage = Image.asset('assets/images/icons/back-arrow2.png', width: 14.w, height: 14.h,);
+  static Image homeImage = Image.asset('assets/images/icons/home.png', width: 26.w, height: 28.h,);
+  static Image speakerImage = Image.asset('assets/images/icons/speaker.png', width: 26.w, height: 28.h,);
+  static Image subtractImage = Image.asset('assets/images/icons/Subtract.png', width: 27.w, height: 27.h,);
+
+  static Image clapImage = Image.asset('assets/images/Clapping hands.png', width: 165.w, height: 165.h,);
+  static Image lockedImage = Image.asset('assets/images/Locked with key.png', width: 113.w, height: 113.h,);
+  static Image partyImage = Image.asset('assets/images/Party popper.png', width: 151.w, height: 151.h,);
+  static Image clockImage = Image.asset('assets/images/clock.png', width: 25.w, height: 25.h,);
+  static Image vectorImage = Image.asset('assets/images/Vector.png', width: 14.w, height: 16.h,);
+  static Image dotImage = Image.asset('assets/images/dot.png', width: 5.w, height: 5.h,);
+  static Image rightImage = Image.asset('assets/images/right.png', width: 24.w, height: 24.h,);
+}
