f1b500c4f3a19b87af74f14d3ce5963910650183
taejun <ptj01036251@gmail.com>
2024-12-05 23:28:50 +0900

    설정 화면 퍼블리싱

---
+++ lib/screen/main_page/setting_screen.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../component/clim_image.dart';
+
+class SettingScreen extends StatefulWidget {
+  const SettingScreen({super.key});
+
+  @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffF1F3F5),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.white,
+            width: 500.w,
+            height: 210.h,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '설정',
+                    style: TextStyle(
+                        fontSize: 32.sp, fontFamily: 'PretendardSemiBold'),
+                  ),
+                  SizedBox(
+                    height: 30.h,
+                  ),
+                  Text(
+                    '내 계정',
+                    style: TextStyle(
+                        fontSize: 16.sp,
+                        color: const Color(0xff838383),
+                        fontFamily: 'PretendardMedeium'),
+                  ),
+                  SizedBox(
+                    height: 10.h,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '내 정보 확인하기',
+                        style: TextStyle(
+                          fontSize: 20.sp,
+                          color: const Color(0xff000000),
+                        ),
+                      ),
+                      IconButton(onPressed: () {}, icon: ClimImage.rightImage)
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '비밀번호 변경하기',
+                        style: TextStyle(
+                          fontSize: 20.sp,
+                          color: const Color(0xff000000),
+                        ),
+                      ),
+                      IconButton(onPressed: () {}, icon: ClimImage.rightImage)
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.h,
+          ),
+          Container(
+            color: Colors.white,
+            width: 500.w,
+            height: 477.h,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  Text(
+                    '로그인',
+                    style: TextStyle(
+                        fontSize: 16.sp,
+                        color: const Color(0xff838383),
+                        fontFamily: 'PretendardMedium'),
+                  ),
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '자동 로그인',
+                        style: TextStyle(
+                            color: const Color(0xff000000),
+                            fontSize: 20.sp,
+                            fontFamily: 'PretendardMedium'),
+                      ),
+                      CupertinoSwitch(
+                        value: isChecked,
+                        onChanged: (value) {
+                          setState(() {
+                            isChecked = value;
+                          });
+                        },
+                        trackColor: const Color(0xff48E892),
+                      ),
+                    ],
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      '로그아웃',
+                      style: TextStyle(
+                          color: const Color(0xffFF5555),
+                          fontFamily: 'PretendardSemiBold',
+                          fontSize: 20.sp),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
