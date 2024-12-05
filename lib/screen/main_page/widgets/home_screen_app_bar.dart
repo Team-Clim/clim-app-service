6974a752aa9124b8f7ad100ba316a330cc20f2d9
taejun <ptj01036251@gmail.com>
2024-12-05 23:26:50 +0900

    홈 화면 앱바 구현

---
+++ lib/screen/main_page/widgets/home_screen_app_bar.dart
@@ -0,0 +1,55 @@
+import 'package:clim/component/clim_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class HomeScreenAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const HomeScreenAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PreferredSize(
+      preferredSize: preferredSize,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(30.r)),
+        ),
+        child: Column(
+          children: [
+            AppBar(
+              backgroundColor: Colors.transparent,
+              automaticallyImplyLeading: false,
+              title: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 27.w),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Text(
+                      '502',
+                      style: TextStyle(
+                        fontSize: 40.sp,
+                        color: const Color(0xff000000),
+                        fontFamily: 'PretendardBold',
+                      ),
+                    ),
+                    Text(
+                      '호',
+                      style: TextStyle(
+                          fontSize: 20.sp,
+                          color: Colors.black,
+                          fontFamily: 'PretendardThin'),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(100.h);
+}
