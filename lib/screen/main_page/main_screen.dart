49816ea89aa835a864d4573b453dca60abe24d0e
taejun <ptj01036251@gmail.com>
2024-12-09 22:27:36 +0900

    refactor: BottomNavigationBar 높이 및 색상 변경

--- lib/screen/main_page/main_screen.dart
+++ lib/screen/main_page/main_screen.dart
@@ -3,6 +3,7 @@ import 'package:clim/screen/main_page/home_screen.dart';
 import 'package:clim/screen/main_page/notification_screen.dart';
 import 'package:clim/screen/main_page/setting_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -41,26 +42,33 @@ class _MainScreenState extends State<MainScreen>
           SettingScreen(),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        showUnselectedLabels: false,
-        showSelectedLabels: false,
-        selectedItemColor: const Color(0xff48E892),
-        unselectedItemColor: const Color(0xffE1E1E1),
-        currentIndex: _index,
-        onTap: (value) {
-          setState(() {
-            _index = value;
-            _tabController.animateTo(value);
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-              icon: _index == 0 ? ClimImage.home2Image : ClimImage.homeImage, label: "home"),
-          BottomNavigationBarItem(
-              icon: _index == 1 ? ClimImage.speaker2Image : ClimImage.speakerImage, label: "speaker"),
-          BottomNavigationBarItem(
-              icon: _index == 2 ? ClimImage.setting2Image : ClimImage.settingImage, label: "subtract"),
-        ],
+      bottomNavigationBar: Container(
+        height: 123.h,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(15.r)),
+        ),
+        child: BottomNavigationBar(
+          showUnselectedLabels: false,
+          showSelectedLabels: false,
+          selectedItemColor: const Color(0xff48E892),
+          unselectedItemColor: const Color(0xffE1E1E1),
+          backgroundColor: const Color(0xffFFFFFF),
+          currentIndex: _index,
+          onTap: (value) {
+            setState(() {
+              _index = value;
+              _tabController.animateTo(value);
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: _index == 0 ? ClimImage.home2Image : ClimImage.homeImage, label: "home"),
+            BottomNavigationBarItem(
+                icon: _index == 1 ? ClimImage.speaker2Image : ClimImage.speakerImage, label: "speaker"),
+            BottomNavigationBarItem(
+                icon: _index == 2 ? ClimImage.setting2Image : ClimImage.settingImage, label: "subtract"),
+          ],
+        ),
       ),
     );
   }
