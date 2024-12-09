915255565d88eb511863ef11b38ebb13cf3a976c
taejun <ptj01036251@gmail.com>
2024-12-09 12:13:43 +0900

    refactor: BottomNavigationBar 수정

--- lib/screen/main_page/main_screen.dart
+++ lib/screen/main_page/main_screen.dart
@@ -1,7 +1,7 @@
+import 'package:clim/component/clim_image.dart';
 import 'package:clim/screen/main_page/home_screen.dart';
 import 'package:clim/screen/main_page/notification_screen.dart';
 import 'package:clim/screen/main_page/setting_screen.dart';
-import 'package:clim/screen/main_page/widgets/bottom_bar.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -11,8 +11,11 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen>  with SingleTickerProviderStateMixin {
+class _MainScreenState extends State<MainScreen>
+    with SingleTickerProviderStateMixin {
   late TabController _tabController;
+
+  int _index = 0;
 
   @override
   void initState() {
@@ -30,15 +33,35 @@ class _MainScreenState extends State<MainScreen>  with SingleTickerProviderState
   Widget build(BuildContext context) {
     return Scaffold(
       body: TabBarView(
-        controller: _tabController,
         physics: const NeverScrollableScrollPhysics(),
-          children: const <Widget>[
-            HomeScreen(),
-            NotificationScreen(),
-            SettingScreen(),
-          ],
+        controller: _tabController,
+        children: const [
+          HomeScreen(),
+          NotificationScreen(),
+          SettingScreen(),
+        ],
       ),
-      bottomNavigationBar: BottomBar(tabController: _tabController,),
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        selectedItemColor: const Color(0xff48E892),
+        unselectedItemColor: const Color(0xffE1E1E1),
+        currentIndex: _index,
+        onTap: (value) {
+          setState(() {
+            _index = value;
+            _tabController.animateTo(value);
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+              icon: _index == 0 ? ClimImage.home2Image : ClimImage.homeImage, label: "home"),
+          BottomNavigationBarItem(
+              icon: _index == 1 ? ClimImage.speaker2Image : ClimImage.speakerImage, label: "speaker"),
+          BottomNavigationBarItem(
+              icon: _index == 2 ? ClimImage.setting2Image : ClimImage.settingImage, label: "subtract"),
+        ],
+      ),
     );
   }
 }
