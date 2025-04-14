da843b359b1213ad01d066ae704da1f7765e2eeb
taejun <ptj01036251@gmail.com>
2025-04-14 23:00:32 +0900

    프로젝트 초기 세팅

---
+++ lib/core/clim/clim_color.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+class ClimColors {
+  ClimColors._();
+
+  static const Color climMint100 = Color(0xFF48E892);
+
+  static const Color climBlack100 = Color(0xFF000000);
+
+  static const Color climWhite100 = Color(0xFFFFFFFF);
+
+  static const Color climRed100 = Color(0xFFFF5555);
+
+  static const Color climGray100 = Color(0xFFD9D9D9);
+  static const Color climGray200 = Color(0xFF7A7C80);
+  static const Color climGray300 = Color(0xFFAAAAAA);
+  static const Color climGray400 = Color(0xFF838383);
+}
---
+++ lib/core/images.dart
@@ -0,0 +1,27 @@
+class Images {
+  Images._();
+
+  static const String climLogo1 = 'assets/images/logos/logo1.png';
+  static const String climLogo2 = 'assets/images/logos/logo2.png';
+
+  //NavgationBar에서 사용되는 아이콘
+  static const String home = 'assets/images/navigator/home.png';
+  static const String speaker = 'assets/images/navigator/speaker.png';
+  static const String person = 'assets/images/navigator/person.png';
+  static const String coloredHome = 'assets/images/navigator/coloredHome.png';
+  static const String coloredSpeaker = 'assets/images/navigator/colored_speaker.png';
+  static const String coloredPerson = 'assets/images/navigator/colored_person.png';
+
+  //소개 페이지에서 사용되는 아이콘
+  static const String page1 = 'assets/images/pages/page1.png';
+  static const String page2 = 'assets/images/pages/page2.png';
+  static const String page3 = 'assets/images/pages/page3.png';
+
+  //기타 아이콘
+  static const String backArrow = 'assets/images/back_arrow.png';
+  static const String clapHands = 'assets/images/clap.png';
+  static const String clock = 'assets/images/clock.png';
+  static const String lock = 'assets/images/lock.png';
+  static const String returnIcon = 'assets/images/return.png';
+  static const String surpriseIcon = 'assets/images/surprise.png';
+}
