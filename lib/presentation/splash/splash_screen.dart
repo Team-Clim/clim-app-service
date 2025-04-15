43949203e195d2e0d03056c48d449caa74dd65cd
taejun <ptj01036251@gmail.com>
2025-04-15 16:33:10 +0900

    feat: 스플래시 화면 퍼블리싱
    스플래시 화면을 퍼블리싱합니다.
    - AnimatedBuilder를 이용해 로고 애니메이션 개발
    - Future.delayed를 이용해 3초 후 화면 자동 이동

--- lib/presentation/splash/splash_screen.dart
+++ lib/presentation/splash/splash_screen.dart
@@ -1,10 +1,72 @@
-import 'package:flutter/material.dart';
+import 'package:clim_app_service/core/images.dart';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacityAnimation;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+    );
+    _scaleAnimation = Tween<double>(begin: 0.8, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.elasticOut),
+    );
+    _controller.forward();
+    Future.delayed(const Duration(seconds: 3), () {
+      context.goNamed('introduce_screen');
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final Size screenSize = MediaQuery.of(context).size;
+    final double logoWidth = screenSize.width * 0.32; // 화면 너비의 40% 크기로 설정
+    final double logoHeight = screenSize.height * 0.35; // 비율에 따라 높이 설정
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFF8FAFC), // 연한 배경색
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Opacity(
+              opacity: _opacityAnimation.value,
+              child: Transform.scale(
+                scale: _scaleAnimation.value,
+                child: Image.asset(
+                  Images.climLogo1,
+                  width: logoWidth,
+                  height: logoHeight,
+                )
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
