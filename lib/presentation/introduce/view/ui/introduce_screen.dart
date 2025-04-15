09a1425756b039381c2afa8a33135121f3aa0b4e
taejun <ptj01036251@gmail.com>
2025-04-15 21:50:46 +0900

    feat: 소개 페이지 조금 구현

--- lib/presentation/introduce/view/ui/introduce_screen.dart
+++ lib/presentation/introduce/view/ui/introduce_screen.dart
@@ -1,12 +1,27 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:clim_app_service/component/clim_layout.dart';
 import 'package:clim_app_service/core/images.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../../../../core/clim/clim_color.dart';
 
-class IntroduceScreen extends StatelessWidget {
+class IntroduceScreen extends StatefulWidget {
   const IntroduceScreen({super.key});
+
+  @override
+  State<IntroduceScreen> createState() => _IntroduceScreenState();
+}
+
+class _IntroduceScreenState extends State<IntroduceScreen> {
+  int activeIndex = 0;
+
+  final List<String> images = [
+    Images.page1,
+    Images.page2,
+    Images.page3,
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -25,9 +40,7 @@ class IntroduceScreen extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(
-              height: 0.04.sh
-            ),
+            SizedBox(height: 0.08.sh),
             Text(
               '기숙사 청소를 쉽게',
               style: textStyle,
@@ -49,9 +62,62 @@ class IntroduceScreen extends StatelessWidget {
               '환영합니다!',
               style: textStyle,
             ),
+            Text(
+              '회원가입 하고 바로 시작해 보세요!',
+              style: textStyle.copyWith(
+                fontSize: 12.sp,
+                color: ClimColors.climGray300,
+              ),
+            ),
+            SizedBox(height: 66.h),
+            CarouselSlider.builder(
+              options: CarouselOptions(
+                initialPage: 0,
+                viewportFraction: 1,
+                enlargeCenterPage: true,
+                onPageChanged: (index, reason) => setState(() {
+                  activeIndex = index;
+                }),
+              ),
+              itemCount: images.length,
+              itemBuilder: (context, index, realIndex) {
+                final path = images[index];
+                return imageSlider(path, index);
+              },
+            ),
+            SizedBox(height: 32.96.h),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: indicator(),
+            ),
+
+            SizedBox(height: 90.7.h),
           ],
         ),
       ),
     );
   }
+
+  Widget imageSlider(path, index) => Container(
+        width: 339.8.w,
+        height: 282.65.h,
+        child: Image.asset(
+          path,
+          fit: BoxFit.fill,
+        ),
+      );
+
+  Widget indicator() => Container(
+        alignment: Alignment.bottomCenter,
+        child: AnimatedSmoothIndicator(
+          activeIndex: activeIndex,
+          count: images.length,
+          effect: JumpingDotEffect(
+            dotHeight: 12.h,
+            dotWidth: 12.w,
+            activeDotColor: ClimColors.climMint100,
+            dotColor: ClimColors.climGray100,
+          ),
+        ),
+      );
 }
